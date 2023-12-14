64fe3ea6dc412f8de4118778d9d6ba5c70794f0d
nohhayeong <151031005+nohhayeong@users.noreply.github.com>
2023-12-14 11:34:20 +0900

    Create 19.kt

---
+++ 1~50/19.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(n: Long): Long {
+        
+        for(i in 1..n){
+            if(i*i==n) return (i+1)*(i+1)            
+        }
+        
+        return -1
+    }
+}
