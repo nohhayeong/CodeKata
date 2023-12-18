2cefe44ee581a568505f63e5c32c06c6381640aa
nohhayeong <151031005+nohhayeong@users.noreply.github.com>
2023-12-18 13:07:14 +0900

    Create 27.kt

---
+++ 1~50/27.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(phone_number: String): String {
+        var answer = ""
+        
+        for(i in 0 until phone_number.length - 4){
+            answer += '*'
+        }
+        answer += phone_number.substring(phone_number.length - 4, phone_number.length)
+        
+        return answer
+    }
+}
