d4fb8de7aba883ae7c3dc9bd9db74b974453b4ad
nohhayeong <151031005+nohhayeong@users.noreply.github.com>
2023-12-26 11:21:08 +0900

    Create 31.kt

---
+++ Level2/31.kt
@@ -0,0 +1,14 @@
+//수박수박수박수박수박수?
+
+class Solution {
+    fun solution(n: Int): String {
+        var answer = ""
+        
+        for(i in 1..n){
+            if(i%2!=0) answer += "수"
+            else answer += "박"
+        }
+        
+        return answer
+    }
+}
