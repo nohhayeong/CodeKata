a431a35c5ac3a196a4b4ae9176d8766128e7e5dc
nohhayeong <151031005+nohhayeong@users.noreply.github.com>
2023-12-27 12:40:35 +0900

    Create 32.kt

---
+++ Level2/32.kt
@@ -0,0 +1,13 @@
+//내적
+
+class Solution {
+    fun solution(a: IntArray, b: IntArray): Int {
+        var answer: Int = 0
+        
+        for(i in 0 until a.size){
+            answer += a[i] * b[i]
+        }
+        
+        return answer
+    }
+}
